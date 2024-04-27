381ff4b3bf47adca748b6a7cfd944aeda47cfe86
DAMunene <muriukimoff6@gmail.com>
2024-04-27 15:17:32 +0300

    allowed hosts to webdent

--- Dental/settings.py
+++ Dental/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-
+ALLOWED_HOSTS = ['webdent.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
